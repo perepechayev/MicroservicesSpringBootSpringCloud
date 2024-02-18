23d00f0c848c0fd9508f24ff79d0925013138da0
Sergey Perepechayev <perepechayev@mail.ru>
2024-02-19 00:09:21 +0300

    fix mapstruct

--- product-service/src/main/java/org/psp/core/product/ProductServiceApplication.java
+++ product-service/src/main/java/org/psp/core/product/ProductServiceApplication.java
@@ -1,13 +1,42 @@
 package org.psp.core.product;
 
+import org.psp.core.product.persistence.ProductEntity;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.event.ContextRefreshedEvent;
+import org.springframework.context.event.EventListener;
+import org.springframework.data.mapping.context.MappingContext;
+import org.springframework.data.mongodb.core.MongoOperations;
+import org.springframework.data.mongodb.core.index.IndexOperations;
+import org.springframework.data.mongodb.core.index.IndexResolver;
+import org.springframework.data.mongodb.core.index.MongoPersistentEntityIndexResolver;
+import org.springframework.data.mongodb.core.mapping.MongoPersistentEntity;
+import org.springframework.data.mongodb.core.mapping.MongoPersistentProperty;
 
 @SpringBootApplication
 @ComponentScan("org.psp")
 public class ProductServiceApplication {
+
+    @Autowired
+    private MongoOperations mongoTemplate;
+
     public static void main(String[] args) {
-        SpringApplication.run(ProductServiceApplication.class, args);
+        ConfigurableApplicationContext ctx = SpringApplication.run(ProductServiceApplication.class, args);
+        String mongodDbHost = ctx.getEnvironment().getProperty("spring.data.mongodb.host");
+        String mongogDbPort = ctx.getEnvironment().getProperty("spring.data.mongodb.port");
+
+    }
+
+    @EventListener(ContextRefreshedEvent.class)
+    public void initIndicesAfterStartrup() {
+        MappingContext<? extends MongoPersistentEntity<?>, MongoPersistentProperty> mappingContext =
+                mongoTemplate.getConverter().getMappingContext();
+        IndexResolver resolver = new MongoPersistentEntityIndexResolver(mappingContext);
+
+        IndexOperations indexOps = mongoTemplate.indexOps(ProductEntity.class);
+        resolver.resolveIndexFor(ProductEntity.class).forEach(e -> indexOps.ensureIndex(e));
     }
 }
