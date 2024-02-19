0dc2bf17220b7ca7dd5b6dca95c083fad7a963e7
Sergey Perepechayev <perepechayev@mail.ru>
2024-02-19 19:32:59 +0300

    product service junit

--- product-service/src/test/java/org/psp/core/product/PersistenceTests.java
+++ product-service/src/test/java/org/psp/core/product/PersistenceTests.java
@@ -2,12 +2,24 @@ package org.psp.core.product;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.psp.api.core.product.Product;
 import org.psp.core.product.persistence.ProductEntity;
 import org.psp.core.product.persistence.ProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.data.mongo.DataMongoTest;
+import org.springframework.dao.DuplicateKeyException;
+import org.springframework.dao.OptimisticLockingFailureException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+import static java.util.stream.IntStream.rangeClosed;
+import static org.junit.jupiter.api.Assertions.*;
 
 @DataMongoTest
 public class PersistenceTests extends MongoDbTestBase {
@@ -18,9 +30,7 @@ public class PersistenceTests extends MongoDbTestBase {
 
     @BeforeEach
     public void setupDb() {
-System.out.println("Deleting all");
         repository.deleteAll();
-System.out.println("Deleted");
         ProductEntity entity = new ProductEntity(1, "n", 1);
         savedEntity = repository.save(entity);
         assertEqualsProduct(entity, savedEntity);
@@ -34,6 +44,77 @@ System.out.println("Deleted");
         assertEqualsProduct(newEntity, foundEntity);
         assertEquals(2, repository.count());
     }
+
+    @Test
+    public void update() {
+        savedEntity.setName("n2");
+        repository.save(savedEntity);
+        ProductEntity foundEntity = repository.findById(savedEntity.getId()).get();
+        assertEquals(1, (long)foundEntity.getVersion());
+        assertEquals("n2", foundEntity.getName());
+    }
+
+    @Test
+    public void delete() {
+        repository.delete(savedEntity);
+        assertFalse(repository.existsById(savedEntity.getId()));
+    }
+
+    @Test
+    public void getProductById() {
+        Optional<ProductEntity> entity = repository.findByProductId(savedEntity.getProductId());
+        assertTrue(entity.isPresent());
+        assertEqualsProduct(savedEntity, entity.get());
+    }
+
+    @Test
+    public void duplicateError() {
+        assertThrows(DuplicateKeyException.class, () -> {
+            ProductEntity entity = new ProductEntity(savedEntity.getProductId(), "n", 1);
+            repository.save(entity);
+        });
+    }
+
+    @Test
+    public void optimisticLockError() {
+        ProductEntity entity1 = repository.findById(savedEntity.getId()).get();
+        ProductEntity entity2 = repository.findById(savedEntity.getId()).get();
+
+        entity1.setName("n1");
+        repository.save(entity1);
+
+        assertThrows(OptimisticLockingFailureException.class, () -> {
+            entity2.setName("n2");
+            repository.save(entity2);
+        });
+
+        ProductEntity updatedEntity = repository.findById(entity1.getId()).get();
+        assertEquals(1, (int)updatedEntity.getVersion());
+        assertEquals("n1", updatedEntity.getName());
+    }
+
+    @Test
+    public void paging() {
+        repository.deleteAll();
+        List<ProductEntity> newProducts = rangeClosed(1001, 1010)
+                .mapToObj(i -> new ProductEntity(i, "name " + i, i))
+                .collect(Collectors.toList());
+        repository.saveAll(newProducts);
+        Pageable nextPage = PageRequest.of(0, 4, Sort.Direction.ASC, "productId");
+        nextPage = testNextPage(nextPage, "[1001, 1002, 1003, 1004]", true);
+        nextPage = testNextPage(nextPage, "[1005, 1006, 1007, 1008]", true);
+        nextPage = testNextPage(nextPage, "[1009, 1010]", false);
+    }
+
+    private Pageable testNextPage(Pageable nextPage, String expectedProductIds, boolean expectsNewPage) {
+        Page<ProductEntity> productPage = repository.findAll(nextPage);
+        assertEquals(expectedProductIds, productPage.getContent().stream()
+                .map(p -> p.getProductId())
+                .collect(Collectors.toList()).toString());
+        assertEquals(expectsNewPage, productPage.hasNext());
+        return productPage.nextPageable();
+    }
+
     private void assertEqualsProduct(ProductEntity expectedEntity, ProductEntity actualEntity) {
         assertEquals(expectedEntity.getId(), actualEntity.getId());
         assertEquals(expectedEntity.getVersion(), actualEntity.getVersion());
